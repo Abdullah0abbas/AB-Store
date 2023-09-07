fb2ba2a5b830ff443a70324f17f6c4af571d2d6b
Abdullah0abbas <abdullah000abbas@gmail.com>
2023-09-07 20:29:06 +0300

    update

--- src/app/shared/home/home.component.ts
+++ src/app/shared/home/home.component.ts
@@ -6,31 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  currentIndex = 0;
-  images: string[] = [
-    '/assets/images/fashion.jpg',
-    '/assets/images/fashion1.webp',
-    '/assets/images/fashion2.jpg'
-  ];
-  currentSlide!: string;
+  // currentIndex = 0;
+  // images: string[] = [
+  //   'assets/images/fashion.jpg',
+  //   'assets/images/fashion1.webp',
+  //   'assets/images/fashion2.jpg'
+  // ];
+  // currentSlide!: string;
 
-  ngOnInit() {
-    this.changeSlide();
-    setInterval(() => this.nextSlide(), 2000);
-  }
+  // ngOnInit() {
+  //   this.changeSlide();
+  //   setInterval(() => this.nextSlide(), 2000);
+  // }
 
-  changeSlide() {
-    this.currentSlide = this.images[this.currentIndex];
-  }
+  // changeSlide() {
+  //   this.currentSlide = this.images[this.currentIndex];
+  // }
 
-  nextSlide() {
-    this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    this.changeSlide();
-  }
+  // nextSlide() {
+  //   this.currentIndex = (this.currentIndex + 1) % this.images.length;
+  //   this.changeSlide();
+  // }
 
-  previousSlide() {
-    this.currentIndex =
-      (this.currentIndex - 1 + this.images.length) % this.images.length;
-    this.changeSlide();
-  }
+  // previousSlide() {
+  //   this.currentIndex =
+  //     (this.currentIndex - 1 + this.images.length) % this.images.length;
+  //   this.changeSlide();
+  // }
 }
